14934dbcbc37c7fb8b27bab0eedf26371c8d9ca9
Carlota Bindner <cev.bindner@gmail.com>
2018-03-14 15:06:36 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, 
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 4, "white", "playful", 1);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 13, "white", "grumpy", 1);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 8, "brown", "gentle", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL, "brown", "ferocious", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL, 15, "brown", "ferocious", 0);
